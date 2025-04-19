604ac3fee532dc04039eb93a2393e8d085bdd74e
Ms012059 <mariasebastian2552@gmail.com>
2025-04-19 15:59:28 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MusicPlayer window;
+    window.show();
+    return app.exec();
+}
